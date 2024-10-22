6c5dac23b731f45eee529f0650406d65117558de
Galvus Damor <galvusdamor@gmail.com>
2024-10-22 21:17:24 +0200

    kissat hack

--- src/decide.c
+++ src/decide.c
@@ -61,21 +61,92 @@ largest_score_unassigned_variable (kissat * solver)
   return res;
 }
 
+
+
+int (*kissat_external_decision) (kissat *, int*) = 0;
+
+void
+kissat_set_external_decision_function(int (*function) (kissat *, int*)){
+	kissat_external_decision = function;
+}
+
+int kissat_get_truth_of_external_var(kissat * solver, int external_var){
+	if (external_var <= 0) return 2; // error
+	if (external_var >= SIZE_STACK (solver->import)) return 2; // error
+	const import *const import = &PEEK_STACK (solver->import, external_var);
+	if (!import->imported) return 2; // error
+	
+	if (import->eliminated){
+		return -2; // variable has been eliminated
+	} else {
+		const unsigned ilit = import->lit;
+		return VALUE (ilit);
+	}
+}
+
+int kissat_internal_from_external_var(kissat * solver, int external_var){
+	if (external_var <= 0) return -1; // error
+	if (external_var >= SIZE_STACK (solver->import)) return -1; // error
+	const import *const import = &PEEK_STACK (solver->import, external_var);
+	if (!import->imported) return -1; // error
+	
+	if (import->eliminated){
+		return -2; // variable has been eliminated
+	}
+
+	return import->lit;
+}
+
+
+int kissat_phase_advice;
+
 unsigned
 kissat_next_decision_variable (kissat * solver)
 {
+  kissat_phase_advice = 0;
+  if (kissat_external_decision != 0){
+	int made_decision;
+  	int chosen_var = kissat_external_decision(solver, &made_decision);
+
+	// if the external procedure has made a suggestion that is valid, take it
+	if (made_decision){
+		int internal_var = kissat_internal_from_external_var(solver,ABS(chosen_var));
+		if (internal_var >= 0){
+			//printf("Taking external advice %d from %d\n", internal_var, chosen_var);
+			
+			if (chosen_var < 0)
+				kissat_phase_advice = -1;
+			else
+				kissat_phase_advice = 1;
+			
+			if (VALUE(internal_var)) printf("ERROR\n", internal_var, chosen_var);
+
+			return IDX(internal_var);
+		}
+	}
+	// otherwise, we default to kissat's own heuristic
+  }
   unsigned res;
   if (solver->stable)
     res = largest_score_unassigned_variable (solver);
   else
     res = last_enqueued_unassigned_variable (solver);
+
   LOG ("next decision %s", LOGVAR (res));
+
   return res;
 }
 
 static inline value
 decide_phase (kissat * solver, unsigned idx)
 {
+  value res = 0;
+  if (kissat_phase_advice){
+  	res = kissat_phase_advice;
+	return res;
+  } 
+  
+  
   bool force = GET_OPTION (forcephase);
 
   value *target;
@@ -96,7 +167,6 @@ decide_phase (kissat * solver, unsigned idx)
   else
     saved = 0;
 
-  value res = 0;
 
   if (!res && target && (res = *target))
     {
--- src/decide.h
+++ src/decide.h
@@ -7,6 +7,8 @@ void kissat_decide (struct kissat *);
 void kissat_internal_assume (struct kissat *, unsigned lit);
 unsigned kissat_next_decision_variable (struct kissat *);
 
+void kissat_set_external_decision_function(int (*function) (struct kissat *, int *));
+
 #define INITIAL_PHASE (GET_OPTION (phase) ? 1 : -1)
 
 #endif
